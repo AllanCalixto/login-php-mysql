2a669be800a93e491895ebc03d453627e5ea59dc
Allan Calixto <allancalixto1@gmail.com>
2020-10-27 22:19:38 -0300

    ..

--- index.php
+++ index.php
@@ -1,60 +1,66 @@
 <?php 
+require_once 'CLASSES/usuarios.php';
+$u = new Usuario();
+?>
 
-	require_once 'usuarios.php';
-	$u = new Usuario;
-
- ?>
-
-<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<title>Projeto Login</title>
 	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="estilo.css">
+	<meta name="viewport" content="width=device-width">
 </head>
 <body>
-	<div id="corpo-form">
-		<h1>Entrar</h1>
-		<form method="POST">
-			<input type="email" placeholder="Usuário" name="email">
-			<input type="password" placeholder="Senha" name="senha">
-			<input type="submit" value="ACESSAR">
-			<a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong>
-		</form>
-	</div>
-
-	<?php 
-
-if(isset($_POST['nome']))
-	{
-		$email = addslashes($_POST['email']);
-		$senha = addslashes($_POST['senha']);
+<div id="corpo-form">
+	<h1>Entrar</h1>
+	<form method="POST">
+		<input type="email" placeholder="Usuário" name="email">
+		<input type="password" placeholder="Senha" name="senha">
+		<input type="submit" value="ACESSAR">
+		<a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+	</form>
+</div>
+<?php
+if(isset($_POST['email']))
+{
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
 	
-		//verificar se os campos foram preenchidos
-		if(!empty($email) && !empty($senha))
+	if(!empty($email) && !empty($senha))
+	{
+		$u->conectar("projeto_login","localhost","allan","allanpsg10");
+		if($u->msgErro == "")
 		{
-			$u->conectar("projeto_login", "localhost", "allan", "allanpsg10");
-			if($u->msgErro == "")
+			if($u->logar($email,$senha))
 			{
-				if($u->logar($email, $senha))
-				{
-					header("areaPrivada.php");
-				}
-
+				header("location: AreaPrivada.php");
+			}
 			else
 			{
-				echo "Email e/ou senha estão incorretos!";
-			}	
+				?>
+				<div class="msg-erro">
+					Email e/ou senha estão incorretos!
+				</div>
+				<?php
+			}
 		}
 		else
 		{
-			echo "Erro: " .$u->msgErro;
-		}	
-		else
-		{
-			echo "Preencha todos os campos!";
-		}	
-	 ?>
-
-	</body>
-	</html>
+			?>
+			<div class="msg-erro">
+				<?php echo "Erro: ".$u->msgErro; ?>
+			</div>
+			<?php
+		}
+	}else
+	{
+		?>
+		<div class="msg-erro">
+			Preencha todos os campos!
+		</div>
+		<?php
+	}
+}
+?>
+</body>
+</html>
